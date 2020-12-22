32a722f4c2534852949cb4da839cc7464785b31b
Captain_A <62920820+aprzygocki@users.noreply.github.com>
2020-12-21 17:24:20 -0800

    Update script.js
    changed http instances to https

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
     $.ajax({
       type: "GET",
       url:
-        "http://api.openweathermap.org/data/2.5/weather?q=" +
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
         searchValue +
         "&appid=f0e8b4ae575247324a95c9d62930ebb2&units=imperial",
       dataType: "json",
@@ -56,7 +56,7 @@ $(document).ready(function () {
         var cardBody = $("<div>").addClass("card-body");
         var img = $("<img>").attr(
           "src",
-          "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
+          "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
         );
 
         // merge and add to page
@@ -76,7 +76,7 @@ $(document).ready(function () {
     $.ajax({
       type: "GET",
       url:
-        "http://api.openweathermap.org/data/2.5/forecast?q=" +
+        "https://api.openweathermap.org/data/2.5/forecast?q=" +
         searchValue +
         "&appid=f0e8b4ae575247324a95c9d62930ebb2&units=imperial",
       dataType: "json",
@@ -101,7 +101,7 @@ $(document).ready(function () {
 
             var img = $("<img>").attr(
               "src",
-              "http://openweathermap.org/img/w/" +
+              "https://openweathermap.org/img/w/" +
                 data.list[i].weather[0].icon +
                 ".png"
             );
@@ -126,7 +126,7 @@ $(document).ready(function () {
     $.ajax({
       type: "GET",
       url:
-        "http://api.openweathermap.org/data/2.5/uvi?appid=f0e8b4ae575247324a95c9d62930ebb2&lat=" +
+        "https://api.openweathermap.org/data/2.5/uvi?appid=f0e8b4ae575247324a95c9d62930ebb2&lat=" +
         lat +
         "&lon=" +
         lon,
